5b7d8cdb9270c98ea4d71ec8ce499f562ffe5c86
Denbeigh Stevens <denbeigh@denbeighstevens.com>
2023-11-10 22:06:38 -0800

    add message param

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-require("@actions/core").setFailed();
+const core = require("@actions/core");
+
+const msg = core.getInput("message", {
+    required: false,
+    trimWhitespace: true,
+});
+core.setFailed(msg);
